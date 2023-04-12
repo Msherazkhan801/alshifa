e7d26cca718292f554f8c3dbffca93c7fa174714
Sheraz Macbook Pro <macctn@macs-MacBook-Pro.local>
2023-04-12 19:58:43 +0500

    remove id

--- src/components/MadicineTable.js
+++ src/components/MadicineTable.js
@@ -20,7 +20,7 @@ export default function BasicTable({item}) {
         <TableBody>
           {item && item.map((row) => (
             <TableRow
-              key={row.id}
+              key={row?.medicineName}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row" align='center'>
